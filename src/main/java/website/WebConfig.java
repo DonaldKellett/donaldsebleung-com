06740ebd188cc4e3f8340ef2fa6f8d1c860dae2d
Donald Sebastian Leung <donaldsebleung@gmail.com>
2021-08-20 23:02:35 +0800

    Add homepage and intro

--- src/main/java/website/WebConfig.java
+++ src/main/java/website/WebConfig.java
@@ -9,5 +9,6 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/").setViewName("index");
+		registry.addViewController("/about").setViewName("about");
 	}
 }
