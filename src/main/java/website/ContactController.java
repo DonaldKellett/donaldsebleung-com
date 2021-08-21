267bf3ba1c0ed8896ce0ae6bc7468cccf4e1a4cf
Donald Sebastian Leung <donaldsebleung@gmail.com>
2021-08-21 19:24:33 +0800

    Add contact page sans contact form

---
+++ src/main/java/website/ContactController.java
@@ -0,0 +1,15 @@
+package website;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/contact")
+public class ContactController {
+	
+	@GetMapping
+	public String showContactForm() {
+		return "contact";
+	}
+}
